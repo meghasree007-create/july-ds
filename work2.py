e9d2a39ae4164c6c0981582aaea6423aeada19fa
megha <meghasree007@gmail.com>
2025-12-14 23:09:36 +0530

    work2.py modified

---
+++ work2.py
@@ -0,0 +1,33 @@
+def mul(a,b):
+    return a*b
+def mul(a,b):
+    return a*b
+def mul(a,b):
+    return a*b
+def mul(a,b):
+    return a*b
+def mul(a,b):
+    return a*b
+def add(a,b):
+    
+      return a+b
+
+def add(a,b):
+    
+      return a+b
+
+def add(a,b):
+    
+      return a+b
+
+def add(a,b):
+    
+      return a+b
+
+def add(a,b):
+    
+      return a+b
+
+def add(a,b):
+    
+      return a+b
